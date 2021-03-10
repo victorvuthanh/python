06e8877d44cbd8c653634dc6701d96f9fdcc2844
Victor Vu <79396055+victorvuthanh@users.noreply.github.com>
2021-03-10 09:04:03 +0300

    Update lesson4_5.py

--- lesson4_5.py
+++ lesson4_5.py
@@ -1,17 +1,18 @@
 # Реализовать формирование списка, используя функцию range() и возможности генератора.
 # В список должны войти четные числа от 100 до 1000 (включая границы). Необходимо получить результат
 # вычисления произведения всех элементов списка. Подсказка: использовать функцию reduce().
-
 from functools import reduce
+from print_table import print_table
 
 def my_func(x, y):
     return x * y
 my_list = [el for el in range(100, 1001) if el % 2 == 0]
 
 print(f'Список четных значений от 100 до 1000:')
-from print_table import print_table
 print_table(my_list, 50, 4)
 
-print(f'Результат перемножения: {reduce(my_func, my_list)}')
+result = str(reduce(my_func, my_list))
+print(f'Результат перемножения:')
+print_table(result, 150, 1)
 
 print('\nКонец программы')
