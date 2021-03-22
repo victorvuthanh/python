33848e7124123ba6bb4a7312a0041cca551e7449
victorvuthanh <vuthanh@gmail.com>
2021-03-22 16:55:54 +0300

    UP1

---
+++ lesson7_1.py
@@ -0,0 +1,55 @@
+'''
+1. Реализовать класс Matrix (матрица). Обеспечить перегрузку конструктора класса (метод __init__()),
+который должен принимать данные (список списков) для формирования матрицы.
+
+Подсказка: матрица — система некоторых математических величин, расположенных в виде прямоугольной схемы.
+Примеры матриц вы найдете в методичке.
+
+Следующий шаг — реализовать перегрузку метода __str__() для вывода матрицы в привычном виде.
+
+Далее реализовать перегрузку метода __add__() для реализации операции сложения двух объектов
+класса Matrix (двух матриц). Результатом сложения должна быть новая матрица.
+
+Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки
+первой матрицы складываем с первым элементом первой строки второй матрицы и т.д.
+'''
+
+from typing import List
+
+
+class Matrix:
+    def __init__(self, matrix_data: List[List]):
+        self.__matrix = matrix_data
+
+        m_rows = len(self.__matrix)
+        self.__matrix_size = frozenset([(m_rows, len(row)) for row in self.__matrix])
+
+        if len(self.__matrix_size) != 1:
+            raise ValueError("Invalid matrix size")
+
+    def __add__(self, other: "Matrix") -> "Matrix":
+        if not isinstance(other, Matrix):
+            raise TypeError(f"'{other.__class__.__name__}' "
+                            f"incompatible object type")
+        if self.__matrix_size != other.__matrix_size:
+            raise ValueError(f"Matrix sizes difference")
+
+        result = []
+
+        for item in zip(self.__matrix, other.__matrix):
+            result.append([sum([j, k]) for j, k in zip(*item)])
+
+        return Matrix(result)
+
+    def __str__(self) -> str:
+        return '\n'.join(['\t'.join(map(str, row)) for row in self.__matrix])
+
+
+if __name__ == '__main__':
+    matrix1 = Matrix([[1, 2], [3, 4]])
+    print(matrix1, '\n')
+
+    matrix2 = Matrix([[10, 20], [30, 40]])
+    print(matrix2, '\n')
+
+    print(matrix1 + matrix2)
